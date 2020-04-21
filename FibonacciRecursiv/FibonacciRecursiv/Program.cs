1da10cc1a7dc7c416df4d991a56ced9bfb70a4b1
Patcas Florian <patcasf12@gmail.com>
2020-04-21 11:51:13 +0300

    FibonacciRecursiv

---
+++ FibonacciRecursiv/FibonacciRecursiv/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FibonacciRecursiv
+{
+	public static class FactorialHelper
+	{
+		//algoritm pentru calcularea Fibonacci(n)
+
+		public static int FibonacciRecursiv(int fibonacciNr)
+		{
+			if (fibonacciNr <= 0)
+			{
+				return 0;
+			}
+			else if (fibonacciNr == 1)
+			{
+				return 1;
+			}
+			else
+			{
+				return FibonacciRecursiv(fibonacciNr - 1) + FibonacciRecursiv(fibonacciNr - 2);
+			}
+
+
+		}
+
+	}		
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine();
+			Console.WriteLine(FactorialHelper.FibonacciRecursiv(7));
+			Console.WriteLine();
+
+		}
+	}
+}
